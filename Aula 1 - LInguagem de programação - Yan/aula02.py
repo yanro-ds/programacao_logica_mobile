5aae1caa59df375c54ea52045f92f42e90966a9d
Yan <158524554+yanro-ds@users.noreply.github.com>
2024-04-16 19:04:27 -0300

    Add files via upload
    upload python

---
+++ Aula 1 - LInguagem de programação - Yan/aula02.py
@@ -0,0 +1,13 @@
+#print("ola mundo, finalmente quebrei a maldiçao")
+
+##
+
+print("Olá bem vindo ao nosso app!")
+
+nome = input("Qual é o seu nome ?:")
+idade = input("Qual é a sua idade ?:")
+gosto = input("Qual é o seu hobbie ?:")
+
+print(f"Olá {nome}, você tem {idade} anos, o seu hobbie é {gosto}")
+
+#print("Seu nome é David, sua idade é 25 anos. Gosta de tocar bateria, na igraja!")
